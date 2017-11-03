a5be07adf671b10b48d8ec40174ada5d5c8f3910
Christian Pointner <equinox@spreadspace.org>
2017-11-03 21:00:01 +0100

    make clock right aligned

--- node.lua
+++ node.lua
@@ -367,7 +367,8 @@ function node.render()
 
     util.draw_correct(CONFIG.logo.ensure_loaded(), 32, 32, 332, 332)
     CONFIG.font:write(364, 182, current_room.name_short, 150, CONFIG.foreground_color.rgba())
-    CONFIG.font:write(WIDTH-300, 202, clock.get(), 130, CONFIG.foreground_color.rgba())
+    local clock_size = CONFIG.font:size(clock.get(), 130)
+    CONFIG.font:write(WIDTH - 32 - clock_size, 202, clock.get(), 130, CONFIG.foreground_color.rgba())
 
     local fov = math.atan2(HEIGHT, WIDTH*2) * 360 / math.pi
     gl.perspective(fov, WIDTH/2, HEIGHT/2, -WIDTH,
