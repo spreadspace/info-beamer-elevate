13b2876d7223cb0065e2f13bae45ae2a5b7cce67
Christian Pointner <equinox@spreadspace.org>
2018-02-18 01:49:09 +0100

    add formated time to schedule.json

--- importer.py
+++ importer.py
@@ -10,26 +10,33 @@ import dateutil.parser
 def get_schedule(api_url, locations, devices, schedule_tz):
     schedule = {}
 
+    def __hour_min(t):
+        return t.strftime("%H:%M")
+
+    def __unix_ts(t):
+        return int(t.strftime("%s"))
+
+    def __new_event(title, track, start, end):
+        e = {'title':  title, 'track': track}
+        e['start'] = __hour_min(start)
+        e['startts'] = __unix_ts(start)
+        e['end'] = __hour_min(end)
+        e['endts'] = __unix_ts(end)
+        return e
+
     now = datetime.now(schedule_tz).replace(minute=0, second=0)
     now_1h = now + timedelta(hours=1)
     now_2h = now + timedelta(hours=2)
     now_3h = now + timedelta(hours=3)
     now_4h = now + timedelta(hours=4)
 
-    e1 = {'title': 'Eroeffnungsshow', 'track': 'discourse'}
-    e1['start'] = now.strftime('%s')
-    e1['end'] = now_1h.strftime('%s')
-    e2 = {'title': 'Bernhard Fleischmann & Band', 'track': 'music'}
-    e2['start'] = now_2h.strftime('%s')
-    e2['end'] = now_3h.strftime('%s')
+    e1 = __new_event('Eroeffnungsshow', 'discourse', now, now_1h)
+    e2 = __new_event('Bernhard Fleischmann & Band', 'music', now_2h, now_3h)
     schedule['orpheum'] = [e1, e2]
 
-    e3 = {'title': 'Meeting of the Secret-Society', 'track': 'discourse'}
-    e3['start'] = now.strftime('%s')
-    e3['end'] = now_2h.strftime('%s')
-    e4 = {'title': 'Nothing to see here!', 'track': 'arts'}
-    e4['start'] = now_3h.strftime('%s')
-    e4['end'] = now_4h.strftime('%s')
-    schedule['forum'] = [e3, e4]
+    e3 = __new_event('Meeting of the Secret-Society', 'discourse', now, now_2h)
+    e4 = __new_event('Nothing to see here!', 'arts', now_2h, now_3h)
+    e5 = __new_event('Kann das Weg?', 'arts', now_3h, now_4h)
+    schedule['forum'] = [e3, e4, e5]
 
     return schedule
