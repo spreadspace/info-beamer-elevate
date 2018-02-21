14dd3fc37523ed7ec250bcba5c25048e1b0ab3dd
Christian Pointner <equinox@spreadspace.org>
2018-02-21 01:05:29 +0100

    some more event entries

--- importer.py
+++ importer.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import traceback
 import urllib2
 import calendar
@@ -29,18 +31,26 @@ def get_schedule(api_url, tracks, locations, devices, schedule_tz):
     now_2h = now + timedelta(hours=2)
     now_3h = now + timedelta(hours=3)
     now_4h = now + timedelta(hours=4)
+    now_5h = now + timedelta(hours=5)
+    now_6h = now + timedelta(hours=6)
+    now_7h = now + timedelta(hours=7)
+    now_8h = now + timedelta(hours=8)
 
-    e1 = __new_event("Eroeffnungsshow", 'discourse', now, now_1h)
+    e1 = __new_event("Eröffnungsshow", 'discourse', now, now_2h)
     e1['subtitle'] = "Welcome to the Elevate Festival 2018"
-    e2 = __new_event("Bernhard Fleischmann & Band", 'music', now_2h, now_3h)
-    schedule['orpheum'] = [e1, e2]
+    e2 = __new_event("Pushmann", 'music', now_3h, now_4h)
+    e3 = __new_event(u"Wälder", 'music', now_4h, now_5h)
+    e4 = __new_event("Bernhard Fleischmann & Band", 'music', now_5h, now_6h)
+    schedule['orpheum'] = [e1, e2, e3, e4]
 
-    e3 = __new_event("Meeting of the Secret-Society", 'discourse', now, now_2h)
-    e3['subtitle'] = "don't tell Donald Trump!!!!"
-    e4 = __new_event("Nothing to see here!", 'arts', now_2h, now_3h)
-    e4['subtitle'] = "this subtitle left intentionally blank"
-    e5 = __new_event("Ist das Kunst?", 'arts', now_3h, now_4h)
-    e5['subtitle'] = "... oder kann das Weg?"
-    schedule['forum'] = [e3, e4, e5]
+    e11 = __new_event("Meeting of the Secret-Society", 'discourse', now, now_1h)
+    e11['subtitle'] = "don't tell Donald Trump!!!!"
+    e12 = __new_event("WTF is going on?", 'discourse', now_1h, now_2h)
+    e12['subtitle'] = "the title is rather self-explanatory"
+    e13 = __new_event("Nothing to see here!", 'arts', now_2h, now_3h)
+    e13['subtitle'] = "this subtitle left intentionally blank"
+    e14 = __new_event("Ist das Kunst?", 'arts', now_3h, now_4h)
+    e14['subtitle'] = "... oder kann das Weg?"
+    schedule['forum'] = [e11, e12, e13, e14]
 
     return schedule
