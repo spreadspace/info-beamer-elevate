af47c8a6c6ff858b3eb04a7479218171931b2582
Christian Pointner <equinox@spreadspace.org>
2018-11-03 02:53:43 +0100

    replaced fancy background with e19 version

--- fancy.lua
+++ fancy.lua
@@ -15,16 +15,27 @@ local QPOSX, QPOSY = 0.4, 0.3   -- querulant base position [(0,0) = center, (1,1
 local QMOVESCALE = 0.20
 local QROTSPEED = 1.0
 
-local PILLAR_TOP_GRANULARITY = 0.03   -- top looks better with larger rows (smaller model scale!)
-local PILLAR_SIDE_GRANULARITY = 0.002 -- sides are better with smaller rows
-
 
 local fancy = {}
 
+----------------------------------------------
+-- utils
 
 local function chaos(t)
   return (exp(sin(t*0.22))*exp(cos(t*0.39))*sin(t*0.3));
 end
+
+local function rotaterad(a, ...)
+    gl.rotate(RADTODEG * a, ...)
+end
+
+local function rotate1(a, ...) -- 0.5 = half rotation, 1 = full rotation
+    gl.rotate(ROT1 * a, ...)
+end
+
+
+----------------------------------------------
+-- querulant
 
 local function queruPos(t)
   local dx = chaos(t * PI)
@@ -32,16 +43,10 @@ local function queruPos(t)
   return QPOSX + QMOVESCALE * dx, QPOSY + QMOVESCALE * dy
 end
 
-local function rotaterad(a, ...)
-    gl.rotate(RADTODEG * a, ...)
-end
-local function rotate1(a, ...) -- 0.5 = half rotation, 1 = full rotation
-    gl.rotate(ROT1 * a, ...)
-end
-
-local function drawqueru(now)
+local function drawQueru(now)
     local ox, oy, x, y = 0.012, 0.01, queruPos(now)
     local s = 1.05
+
     -- shadow
     -- PUSH()
     --     gl.translate(x+ox, y+oy)
@@ -50,6 +55,7 @@ local function drawqueru(now)
     --     gl.translate(QSIZE/-2, QSIZE/-2) -- center rotation point
     --     fancy.res.shadow:draw(0,0,QSIZE,QSIZE)
     -- POP()
+
     -- querulant
     PUSH()
         gl.translate(x,y)
@@ -59,104 +65,10 @@ local function drawqueru(now)
     POP()
 end
 
-local function drawside(tex, ...)
-    tex:draw(-0.5,-0.5,0.5,0.5,1, ...)
-end
 
-
-local function drawpillar(tex)
-    PUSH()
-    gl.scale(0.18, 0.8, 0.18)
-
-        --[[
-        PUSH() -- top
-            rotate1(0.5, 0, 1, 0)
-            rotate1(0.5, 1, 0, 0)
-            gl.translate(0, 0, 0.5) -- -0.5 to draw bottom
-            drawside(tex)
-        POP()
-        ]]
-
-        PUSH() -- front
-            gl.scale(-1, 1, 1) -- this makes sure the textures line up and corner transition goes nicely
-            gl.translate(0, 0, 0.5)
-            drawside(tex, 0, 0, 0.5, 1) -- first half of X-axis UV-coord space
-        POP()
-
-        PUSH() -- side
-            rotate1(0.5, 0, 1, 0)
-            gl.translate(0, 0, -0.5)
-            drawside(tex, 0.5, 0, 1, 1)  -- second half of X-axis UV-coord space
-        POP()
-    POP()
-end
-
-local function stripe(tex, rot, cx, cy, ox, oy, sx, sy)
-    PUSH()
-        gl.translate(cx, cy)
-        rotate1(rot, 0, 0, 1)
-        gl.translate(ox, oy)
-        gl.scale(sx or 0.13, sy or 0.5)
-        drawside(tex, 0.5, 0, 1, 1)  -- second half of X-axis UV-coord space
-    POP()
-end
-
--- test function to try good looking rotation parameters...
-local function lolpillar(tex, now, xo, yo, twist)
-    twist = twist or 0.2
-    local onow = now + xo
-    local s, s2, c = sin(onow), sin(onow*0.5), cos(onow*0.66)
-    local sslow = sin(now*0.2 + xo*5)
-    local stw = cos(onow*0.66 + xo*3.3)*0.03 + twist + 0.03*sin(now*0.13 + 5*xo)
-    PUSH()
-        gl.translate(xo, yo, 0)
-        gl.translate(0, 0.06*sslow + 0.42 + 0.031*chaos((now+(10*yo))*0.4), 0)
-        rotate1(-0.05, 0, 0, 1)       -- tilt left/right / roll
-        rotate1(0.01*c-0.1, -1, 1, 0) -- tilt to viewer / pitch
-        rotate1(stw, 0, 1, 0)  -- twist / yaw
-        local ss = s * 0.01 + 1.5
-        gl.scale(ss, ss, ss)
-        drawpillar(tex)
-    POP()
-end
-
-local function drawpillars(now)
-    local res = fancy.res
-    res.fancy_pillar:use{time=now % 1337.4223}
-
-    lolpillar(res.fancy_noise, now,  -0.45,   0.6,  0.33)
-    --lolpillar(res.noise6, now,  0.4,   -0.12,  0.33)
-    lolpillar(res.fancy_noise1, now,  0.3,   0.08,  0.2)
-    --lolpillar(res.noise, now,  0.0,   -0.05,  0.5)
-    lolpillar(res.fancy_noise3, now, -0.28,   0.35,  0.47)
-    lolpillar(res.fancy_noise2, now,  0.1,  0.2,   0.3)
-    lolpillar(res.fancy_noise, now,  0.78,  0.1,   0.2)
-    lolpillar(res.fancy_noise5d, now,  0.26,   0.6,  0.17)
-
-    lolpillar(res.fancy_noise3, now,  0.63,   0.3,   0.25)
-    --lolpillar(res.fancy_noise4, now,  0.7,   0.15,  0.15)
-    --lolpillar(res.fancy_noise7, now, -0.15,  0.45,  0.13)
-    --lolpillar(res.fancy_noise8a, now,  0.8,   0.3,  0.22)
-    --lolpillar(res.fancy_noise1, now,  0.42,   0.5,  0.42)
-
-
-
-    local slownow = 0.3*now
-
-    --stripe(res.noise8a, -0.05, -0.25, 0.3, 0, 0.03*sin(slownow))
-    stripe(res.fancy_noise8a, -0.05, -0.25, 0.5, 0, 0.03*sin(slownow+0.7))
-    stripe(res.fancy_noise8a, -0.05, -0.45, 0.7, 0, 0.03*sin(slownow+1.4))
-
-    stripe(res.fancy_noise8a, -0.05, 0.18, 0.4, 0, 0.03*sin(slownow))
-    stripe(res.fancy_noise8a, -0.05, 0.4, 0.45, 0, 0.03*sin(slownow+0.7))
-    stripe(res.fancy_noise8a, -0.05, 0.62, 0.5, 0, 0.03*sin(slownow+1.4))
-
-
-    res.fancy_pillar:deactivate()
-end
-
-
+----------------------------------------------
 -- modes: "minimal", fancy".
+
 function fancy.render(mode, aspect)
     aspect = aspect or (WIDTH / HEIGHT)
     local now = sys.now()
@@ -164,7 +76,7 @@ function fancy.render(mode, aspect)
 
     if mode == "minimal" then
         res.fancy_minimalbg:draw(0, 0, WIDTH, HEIGHT)
-    elseif  mode == "fancy" then
+    elseif mode == "fancy" then
         res.fancy_bgcolor:draw(0, 0, WIDTH, HEIGHT)
 
         local fov = math.atan2(HEIGHT, WIDTH*2) * 360 / math.pi
@@ -176,7 +88,7 @@ function fancy.render(mode, aspect)
         if fancy.fixaspect then
             fancy.fixaspect(aspect)
         end
-        drawpillars(now)
+        -- TODO: draw fancy animation
     end
 
     if mode == "fancy" or mode == "minimal" then
@@ -186,8 +98,9 @@ function fancy.render(mode, aspect)
         if fancy.fixaspect then
             fancy.fixaspect(aspect)
         end
-        drawqueru(now)
+        drawQueru(now)
     end
 end
 
+----------------------------------------------
 return fancy
