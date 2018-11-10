607821e8c06364f6fdb3d85c96fb27a950ae7881
Christian Pointner <equinox@spreadspace.org>
2018-11-10 02:55:34 +0100

    remove useless importer class

--- importer.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from waffel.waffel import Waffel
-
-
-def get_schedule(api_url, tracks, locations, devices, schedule_tz):
-    waffel = Waffel(api_url, tracks, locations, schedule_tz)
-    schedule = waffel.get_events()
-    return schedule
